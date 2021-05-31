8dea3b4e9b3a3a5b966e4dd8b779a204bd00bee1
Spaceman <farooque258@gmail.com>
2021-05-31 16:41:38 +0530

    ADD(graph): added bfs graph traversal using iterative and recursive approach, refactored existing traversal

--- src/com/spacey/ps/dsa/graph/DFSAdjacencyListIterative.java
+++ src/com/spacey/ps/dsa/graph/DFSAdjacencyListIterative.java
@@ -6,13 +6,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * An implementation of a iterative DFS with an adjacency list
+ * 
+ * Time Complexity: O(V + E)
+ * 
+ * @author Spacey4uq
+ *
+ */
 public class DFSAdjacencyListIterative {
 
 	static class Edge {
 		int from;
 		int to;
 		int cost;
-		
+
 		public Edge(int from, int to, int cost) {
 			super();
 			this.from = from;
@@ -20,30 +28,40 @@ public class DFSAdjacencyListIterative {
 			this.cost = cost;
 		}
 	}
-	
+
+	// Perform a depth first search on a graph with n nodes
+	// from a starting point to count the number of nodes
+	// in a given component.
 	static int dfs(Map<Integer, List<Edge>> graph, int start, int n) {
-		boolean visited[] = new boolean[n];
+
 		int count = 0;
+		boolean[] visited = new boolean[n];
 		Stack<Integer> stack = new Stack<>();
 
 		// Start by visiting the starting node
 		stack.push(start);
-		while(!stack.isEmpty()) {
+		visited[start] = true;
+		System.out.print("DFS Traversal at node " + start + ": ");
+		
+		while (!stack.isEmpty()) {
 			int node = stack.pop();
-			visited[node] = true;
+			System.out.print(node + " ");
 			count++;
 			List<Edge> edges = graph.get(node);
-			if(edges != null) {
-		        for (Edge edge : edges) {
-		            if (!visited[edge.to]) {
-		              stack.push(edge.to);
-		            }
-		        }			
+
+			if (edges != null) {
+				for (Edge edge : edges) {
+					if (!visited[edge.to]) {
+						stack.push(edge.to);
+						visited[edge.to] = true;
+					}
+				}
 			}
 		}
+
 		return count;
 	}
-	
+
 	public static void main(String[] args) {
 	    // Create a fully connected graph
 	    //           (0)
@@ -57,27 +75,29 @@ public class DFSAdjacencyListIterative {
 	    //        1 \   / 6
 	    //           > <
 	    //           (3)
-	    int numNodes = 5;
-	    Map<Integer, List<Edge>> graph = new HashMap<>();
-	    addDirectedEdge(graph, 0, 1, 4);
-	    addDirectedEdge(graph, 0, 2, 5);
-	    addDirectedEdge(graph, 1, 2, -2);
-	    addDirectedEdge(graph, 1, 3, 6);
-	    addDirectedEdge(graph, 2, 3, 1);
-	    addDirectedEdge(graph, 2, 2, 10); // Self loop
-		
-	    long nodeCount = dfs(graph, 0, numNodes);
-	    System.out.println("DFS node count starting at node 0: " + nodeCount);
-	    if (nodeCount != 4) System.err.println("Error with DFS");
+		int numNodes = 5;
+		Map<Integer, List<Edge>> graph = new HashMap<>();
+		addDirectedEdge(graph, 0, 1, 4);
+		addDirectedEdge(graph, 0, 2, 5);
+		addDirectedEdge(graph, 1, 2, -2);
+		addDirectedEdge(graph, 1, 3, 6);
+		addDirectedEdge(graph, 2, 3, 1);
+		addDirectedEdge(graph, 2, 2, 10); // Self loop
 
-	    nodeCount = dfs(graph, 4, 5);
-	    System.out.println("DFS node count starting at node 4: " + nodeCount);
-	    if (nodeCount != 1) System.err.println("Error with DFS");
+		long nodeCount = dfs(graph, 0, numNodes);
+		System.out.println("\nDFS node count starting at node 0: " + nodeCount);
+		if (nodeCount != 4)
+			System.err.println("Error with DFS");
+
+		nodeCount = dfs(graph, 4, 5);
+		System.out.println("\nDFS node count starting at node 4: " + nodeCount);
+		if (nodeCount != 1)
+			System.err.println("Error with DFS");
 	}
 
 	private static void addDirectedEdge(Map<Integer, List<Edge>> graph, int from, int to, int cost) {
 		List<Edge> edges = graph.get(from);
-		if(edges == null) {
+		if (edges == null) {
 			edges = new ArrayList<>();
 			graph.put(from, edges);
 		}
--- src/com/spacey/ps/dsa/graph/DFSAdjacencyListRecursive.java
+++ src/com/spacey/ps/dsa/graph/DFSAdjacencyListRecursive.java
@@ -5,6 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * An implementation of a recursive approach to DFS
+ * 
+ * Time Complexity: O(V + E)
+ * 
+ * @author Spacey4uq
+ *
+ */
 public class DFSAdjacencyListRecursive {
 
 	static class Edge {
@@ -21,25 +29,30 @@ public class DFSAdjacencyListRecursive {
 
 	}
 
+	// Perform a depth first search on the graph counting
+	// the number of nodes traversed starting at some position
 	static long dfs(Map<Integer, List<Edge>> graph, int at, boolean[] visited) {
-	    // We have already visited this node
-	    if (visited[at]) return 0L;
 
-	    // Visit this node
-	    visited[at] = true;
-	    long count = 1;
-		
-	    // Visit all edges adjacent to where we're at
-	    List<Edge> edges = graph.get(at);
-	    if (edges != null) { // since there can even exist a node without an edge
-	      for (Edge edge : edges) {
-	        count += dfs(graph, edge.to, visited);
-	      }
-	    }
+		// We have already visited this node
+		if (visited[at])
+			return 0L;
+
+		// Visit this node
+		visited[at] = true;
+		long count = 1;
+		System.out.print(at + " ");
+
+		// Visit all edges adjacent to where we're at
+		List<Edge> edges = graph.get(at);
+		if (edges != null) {
+			for (Edge edge : edges) {
+				count += dfs(graph, edge.to, visited);
+			}
+		}
 
 		return count;
 	}
-	
+
 	public static void main(String[] args) {
 	    // Create a fully connected graph
 	    //           (0)
@@ -53,29 +66,32 @@ public class DFSAdjacencyListRecursive {
 	    //        1 \   / 6
 	    //           > <
 	    //           (3)
-	    int numNodes = 5;
-	    Map<Integer, List<Edge>> graph = new HashMap<>();
-	    addDirectedEdge(graph, 0, 1, 4);
-	    addDirectedEdge(graph, 0, 2, 5);
-	    addDirectedEdge(graph, 1, 2, -2);
-	    addDirectedEdge(graph, 1, 3, 6);
-	    addDirectedEdge(graph, 2, 3, 1);
-	    addDirectedEdge(graph, 2, 2, 10); // Self loop
-	    
-	    long nodeCount = dfs(graph, 0, new boolean[numNodes]);
-	    System.out.println("DFS node count starting at node 0: " + nodeCount);
-	    if (nodeCount != 4) System.err.println("Error with DFS");
+		int numNodes = 5;
+		Map<Integer, List<Edge>> graph = new HashMap<>();
+		addDirectedEdge(graph, 0, 1, 4);
+		addDirectedEdge(graph, 0, 2, 5);
+		addDirectedEdge(graph, 1, 2, -2);
+		addDirectedEdge(graph, 1, 3, 6);
+		addDirectedEdge(graph, 2, 3, 1);
+		addDirectedEdge(graph, 2, 2, 10); // Self loop
 
-	    nodeCount = dfs(graph, 4, new boolean[numNodes]);
-	    System.out.println("DFS node count starting at node 4: " + nodeCount);
-	    if (nodeCount != 1) System.err.println("Error with DFS");
+		System.out.print("DFS Traversal at node " + 0 + ": ");
+		long nodeCount = dfs(graph, 0, new boolean[numNodes]);
+		System.out.println("\nDFS node count starting at node 0: " + nodeCount);
+		if (nodeCount != 4)
+			System.err.println("Error with DFS");
+
+		System.out.print("DFS Traversal at node " + 4 + ": ");
+		nodeCount = dfs(graph, 4, new boolean[numNodes]);
+		System.out.println("\nDFS node count starting at node 4: " + nodeCount);
+		if (nodeCount != 1)
+			System.err.println("Error with DFS");
 	}
-
 
 	// Helper method to setup graph
 	private static void addDirectedEdge(Map<Integer, List<Edge>> graph, int from, int to, int cost) {
 		List<Edge> edges = graph.get(from);
-		if(edges == null) {
+		if (edges == null) {
 			edges = new ArrayList<>();
 			graph.put(from, edges);
 		}
